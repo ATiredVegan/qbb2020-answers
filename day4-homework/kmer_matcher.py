5c759b3d334c70d822d9e4aea5f1761da1347c4e
Josh Derrick <jderric4@jhu.edu>
2020-09-03 19:12:26 -0400

    Made it so was actually accepting user input. Also added comments.

--- day4-homework/kmer_matcher.py
+++ day4-homework/kmer_matcher.py
@@ -3,21 +3,25 @@
 from fasta_iterator_class import FASTAReader
 
 import sys
+#gest user import
 target_name=sys.argv[1]
 query_name=sys.argv[2]
 k = int(sys.argv[3])
 
-target=FASTAReader(open("subset.fa"))
-
+#imports files from user command 
+target=FASTAReader(open(target_name))
+query=FASTAReader(open(query_name))
 
 target_kmers={}
+#iterates through target and establishes a dictionary with each unique location in each of the target sequences
 for seq_id, sequence in target:
     for i in range(0, len(sequence) - k+1):
         kmer = sequence[i:i + k].upper()
         target_kmers.setdefault(kmer, [])
         target_kmers[kmer].append((i,seq_id))
-query=FASTAReader(open("droYak2_seq.fa"))
+#format output string
 output=["target_sequence_name \t target_start \t query_start \t k-mer"]
+#iterates through query and tries to find matches. Each match=newline
 for seq_id,sequence in query:
     for start in range(0,len(sequence)-k+1):
         kmer=sequence[start:start+k].upper()
@@ -25,7 +29,7 @@ for seq_id,sequence in query:
             for entry in target_kmers[kmer]:
                 output.append("\n"+entry[1]+"\t"+str(entry[0])+"\t"+str(start)+"\t"+kmer)
 
-                
+#returns first 100 lines.                
 with open('alignment.dat','w') as f: 
     for line in output[:1001]:
         f.write("".join(line))
